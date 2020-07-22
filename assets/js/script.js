04b5908f349dbf578b5c3c377bd58c835785896d
MaxwelSantana <maxwelsant96@icloud.com>
2020-07-21 21:35:30 -0300

    added side menu list of cats

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,39 +2,87 @@ const cats = [
     {
         id: 1,
         name: 'Joe',
-        photo: './assets/images/joe-cat.jpg'
+        photo: './assets/images/joe-cat.jpg',
+        count: 0
     },
     {
         id: 2,
         name: 'Lady',
-        photo: './assets/images/lady-cat.jpg'
+        photo: './assets/images/lady-cat.jpg',
+        count: 0
+    },
+    {
+        id: 3,
+        name: 'Simba and Nala',
+        photo: './assets/images/simba-e-nala.jpg',
+        count: 0
+    },
+    {
+        id: 4,
+        name: 'Lady',
+        photo: './assets/images/lady-cat.jpg',
+        count: 0
+    },
+    {
+        id: 5,
+        name: 'Simba and Nala',
+        photo: './assets/images/simba-e-nala.jpg',
+        count: 0
     }
 ];
 
-var createCatsElement = function () {
-    let main = document.getElementById('cats');
+const createCatList = function () {
+    let catListElm  = document.getElementById('catList');
     cats.forEach(cat => {
-        let catSection = document.createElement('section');
-        let catName = document.createElement('h2');
-        let catCounter = document.createElement('span');
-        let catImg = document.createElement('img');
+        let catItem = document.createElement('li');
+        let catImg  = document.createElement('img');
 
-        catName.innerText = `${cat.name} count clicks: `;
-        catCounter.id = cat.id;
-        catCounter.innerText = 0;
-        catImg.id = cat.id;
+        catItem.classList   = 'cat-list__item';
+        catImg.classList    = 'cat-list__img';
         catImg.src = cat.photo;
 
-        catImg.addEventListener('click', function (event) {
-            let id = event.target.id;
-            let countElem = document.getElementById(id);
-            countElem.innerHTML = parseInt(countElem.innerHTML) ? parseInt(countElem.innerHTML) + 1 : 1;
-        });
+        catImg.addEventListener('click', (function (id) {
+            return function () {
+                createMainCatContent(id);
+            }
+        })(cat.id));
 
-        catName.appendChild(catCounter);
-        catSection.appendChild(catName);
-        catSection.appendChild(catImg);
-        main.appendChild(catSection);
+        catItem.appendChild(catImg)
+        catListElm.appendChild(catItem);
     });
 }
-createCatsElement();
+
+const createMainCatContent = function (id) {
+    const cat = cats.find(c => c.id == id);
+    if (!cat) return;
+
+    let catContent  = document.getElementById('catContent');
+    let catName     = document.createElement('h2');
+    let catCounter  = document.createElement('span');
+    let catImg      = document.createElement('img');
+    
+    catContent.innerHTML    = '';
+    catName.innerText       = `${cat.name} count clicks: `;
+    catCounter.id           = cat.id;
+    catCounter.innerText    = cat.count;
+    catImg.classList        = 'cat-content__img';
+    catImg.src              = cat.photo;
+
+    catImg.addEventListener('click', (function(id) {
+        return function (event) {
+            let catObj = cats.find(c => c.id == id);
+            if (!catObj) return;
+
+            catObj.count += 1;
+            let catCounter  = document.getElementById(id);
+            catCounter.innerText = catObj.count;
+        }
+    })(cat.id));
+
+    catName.appendChild(catCounter);
+    catContent.appendChild(catImg);
+    catContent.appendChild(catName);
+}
+
+createCatList();
+createMainCatContent(cats[0].id);
