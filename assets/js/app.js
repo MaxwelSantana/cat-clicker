06b5e4b2567c1ea6e63ac967d677dc60cd07511a
MaxwelSantana <maxwelsant96@icloud.com>
2020-07-26 10:42:17 -0300

    added style anda admin mode

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,19 +32,35 @@
             model.currentCat = model.cats[0];
             catListView.init();
             catCounterView.init();
+            catAdminView.init();
         },
-        setCurrentCat: function(idCat) {
+        setCurrentCat: function (idCat) {
             model.currentCat = model.cats.find(cat => cat.id == idCat);
+            catCounterView.render();
+            catAdminView.render();
         },
-        getCurrentCat: function() {
+        getCurrentCat: function () {
             return model.currentCat;
+        },
+        updateCat: function (id, name, photo, count) {
+            let cat = model.cats.find(cat => cat.id == id);
+            let hasChangedPhoto = cat.photo != photo;
+            cat.id = id;
+            cat.name = name;
+            cat.photo = photo;
+            cat.count = count;
+            if(hasChangedPhoto)
+                catListView.render();
+            catCounterView.render();
+            catAdminView.render();
         },
         getCats: function () {
             return model.cats;
         },
         incrementCounter: function () {
-            model.currentCat.count = model.currentCat.count+1;
+            model.currentCat.count = model.currentCat.count + 1;
             catCounterView.render();
+            catAdminView.render();
         }
     };
 
@@ -55,6 +71,7 @@
         },
         render: function () {
             let cats = octopus.getCats();
+            this.catListElm.innerHTML = '';
             cats.forEach(cat => {
                 let catItem = document.createElement('li');
                 let catImg = document.createElement('img');
@@ -66,7 +83,6 @@
                 catImg.addEventListener('click', (function (idCat) {
                     return function () {
                         octopus.setCurrentCat(idCat);
-                        octopus.incrementCounter();
                     }
                 })(cat.id));
 
@@ -79,12 +95,12 @@
     const catCounterView = {
         init: function () {
             this.catContent = document.getElementById('catContent');
-            this.catName    = document.getElementById('catName');
+            this.catName = document.getElementById('catName');
             this.catCounter = document.getElementById('catCounter');
-            this.catImg     = document.getElementById('catImage');
+            this.catImg = document.getElementById('catImage');
 
-            this.catImg.addEventListener('click', function() {
-                octopus.incrementCounter();    
+            this.catImg.addEventListener('click', function () {
+                octopus.incrementCounter();
             });
 
             this.render();
@@ -92,12 +108,50 @@
 
         render: function () {
             let cat = octopus.getCurrentCat();
-            
+
             this.catCounter.id = cat.id;
             this.catCounter.innerText = cat.count;
             this.catName.innerHTML = `${cat.name} count clicks: ${this.catCounter.outerHTML}`;
             this.catImg.classList = 'cat-content__img';
             this.catImg.src = cat.photo;
+        }
+    }
+
+    const catAdminView = {
+        init: function () {
+            this.id = document.getElementById('idCat');
+            this.name = document.getElementById('name');
+            this.url = document.getElementById('url');
+            this.clicks = document.getElementById('clicks');
+            this.formAdmin = document.getElementById('formAdmin');
+            this.adminButton = document.getElementById('adminButton');
+            this.catMain = document.getElementById('catMain');
+
+            this.formAdmin.addEventListener('submit', function (event) {
+                octopus.updateCat(this.id.value, this.name.value, this.url.value, this.clicks.value);
+                octopus.setCurrentCat(this.id.value);
+                event.preventDefault();
+            });
+
+            this.adminButton.addEventListener('click', function () {
+                catMain.classList.toggle('cat-content__main--open-admin');
+            })
+
+            this.render();
+        },
+        render: function () {
+            this.closeAdmin();
+            setTimeout(() => {
+                let cat = octopus.getCurrentCat();
+
+                this.id.value       = cat.id;
+                this.name.value     = cat.name;
+                this.url.value      = cat.photo;
+                this.clicks.value   = cat.count;
+            }, 300);
+        },
+        closeAdmin: function () {
+            catMain.classList.remove('cat-content__main--open-admin');
         }
     }
 
